e9977e6901da48efe255cca143ddfcb248f3fe80
Anil <anilkumawat@gmail.com>
2025-05-15 12:25:36 +0530

    first commit testing

---
+++ modules/outputs.tf
@@ -0,0 +1,15 @@
+output "instance_public_ip" {
+  description = "public IP of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+
+
+
+
+
---
+++ modules/variables.tf
@@ -0,0 +1,39 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key pair for ec2 access"
+  type        = string
+  default     = "ansiblekey"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+ 
