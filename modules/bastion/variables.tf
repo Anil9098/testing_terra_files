60e6c98e0b9ca9a27dc31cab15245d559c443d94
Anil <anilkumawat@gmail.com>
2025-05-16 14:04:23 +0530

    commit

--- modules/bastion/variables.tf
+++ modules/bastion/variables.tf
@@ -27,7 +27,7 @@ data "aws_ami" "ubuntu" {
   owners      = ["099720109477"]
 }
 
-variable "testing24" {
+variable "testing27" {
   type        = string
   description = "test variable"
 }
--- templates/demo/variables.tf
+++ templates/demo/variables.tf
@@ -43,7 +43,7 @@ variable "testing8" {
   type        = string
 }
  
-variable "testing47" {
+variable "testing48" {
   description = "testing"
   type        = string
 }
